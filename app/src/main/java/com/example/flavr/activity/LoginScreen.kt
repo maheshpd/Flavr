4afd45bc3dae6cfff208582e75d2045a87e4ec7a
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-09-14 19:14:54 +0530

    login function done

--- app/src/main/java/com/example/flavr/activity/LoginScreen.kt
+++ app/src/main/java/com/example/flavr/activity/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.flavr.activity
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
@@ -12,15 +13,15 @@ import kotlinx.android.synthetic.main.activity_login_screen.*
 
 class LoginScreen : AppCompatActivity() {
 
-    private lateinit var mAuth : FirebaseAuth
-
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var dialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
 
         mAuth = FirebaseAuth.getInstance()
-
+        dialog = ProgressDialog(this)
 
         login_btn.setOnClickListener {
 
@@ -28,24 +29,24 @@ class LoginScreen : AppCompatActivity() {
             val password = login_password_edt.text.toString().trim()
 
             if (email.isEmpty()) {
-                KCustomToast.infoToast(this,"Email Required",Gravity.CENTER)
+                KCustomToast.infoToast(this, "Email Required", Gravity.CENTER)
                 login_email_edt.requestFocus()
                 return@setOnClickListener
             }
 
             if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                KCustomToast.infoToast(this,"Valid Email Required",Gravity.CENTER)
+                KCustomToast.infoToast(this, "Valid Email Required", Gravity.CENTER)
                 login_email_edt.requestFocus()
                 return@setOnClickListener
             }
 
             if (password.isEmpty() || password.length < 6) {
-                KCustomToast.infoToast(this,"6 char password required",Gravity.CENTER)
+                KCustomToast.infoToast(this, "6 char password required", Gravity.CENTER)
                 login_password_edt.requestFocus()
                 return@setOnClickListener
             }
 
-            loginUser(email,password)
+            loginUser(email, password)
 
         }
 
@@ -53,9 +54,27 @@ class LoginScreen : AppCompatActivity() {
     }
 
     private fun loginUser(email: String, password: String) {
+        dialog.setMessage("Please wait...")
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.show()
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    dialog.dismiss()
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    }
+                    startActivity(intent)
+                } else {
+                    dialog.dismiss()
+                    task.exception?.message?.let {
+                        KCustomToast.infoToast(this, it, Gravity.CENTER)
+                    }
+                }
+
+            }
 
 
     }
-
 
 }
