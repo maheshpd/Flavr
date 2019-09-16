295cf4e2348cf74bb79eaa72e63991706b478a41
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-09-17 01:40:25 +0530

    error in sign out

--- app/src/main/java/com/example/flavr/fragment/Profile.kt
+++ app/src/main/java/com/example/flavr/fragment/Profile.kt
@@ -1,24 +1,42 @@
 package com.example.flavr.fragment
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.flavr.R
+import com.example.flavr.activity.OptionScreen
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class Profile : Fragment() {
 
+    private lateinit var mAuth: FirebaseAuth
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        signout_btn.setOnClickListener {
+            mAuth.signOut()
+
+            val intent = Intent(context, OptionScreen::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
 
