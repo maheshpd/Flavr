35cd8c724fb563a5d09bb16642f66420b53ce024
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-09-17 01:30:48 +0530

    Splash Screen Done

--- app/src/main/java/com/example/flavr/activity/Splash.kt
+++ app/src/main/java/com/example/flavr/activity/Splash.kt
@@ -1,31 +1,27 @@
 package com.example.flavr.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.example.flavr.R
 import com.google.firebase.auth.FirebaseAuth
 
 class Splash : AppCompatActivity() {
 
-    private var mDelayHandler: Handler? = null
-    private val SPLASH_DELAY: Long = 3000 //3 second
-
-    private lateinit var mAuth: FirebaseAuth
-
-    internal val mRunnable: Runnable = Runnable {
-        if (isFinishing) {
-            val intent = Intent(applicationContext, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }else {
-            val intent = Intent(applicationContext, OptionScreen::class.java)
-            startActivity(intent)
-            finish()
-        }
-    }
+//    private var mDelayHandler: Handler? = null
+//    private val SPLASH_DELAY: Long = 3000 //3 second
+//
+//    private lateinit var mAuth: FirebaseAuth
+//
+//    internal val mRunnable: Runnable = Runnable {
+//        if (isFinishing) {
+//
+//        }else {
+//
+//        }
+//    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,20 +29,31 @@ class Splash : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_splash)
 
-        //Initialize the Handler
-        mDelayHandler= Handler()
+//        //Initialize the Handler
+//        mDelayHandler= Handler()
 
         //Navigate with delay
-        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+//        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+        Handler().postDelayed({
+            if (FirebaseAuth.getInstance().currentUser == null) {
+                val intent = Intent(applicationContext, OptionScreen::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 3000)
     }
 
-    override fun onDestroy() {
-        if (mDelayHandler != null) {
-            mDelayHandler?.removeCallbacks(mRunnable)
-        }
-
-        super.onDestroy()
-    }
+//    override fun onDestroy() {
+//        if (mDelayHandler != null) {
+//            mDelayHandler?.removeCallbacks(mRunnable)
+//        }
+//
+//        super.onDestroy()
+//    }
 
 
 }
